15e5c9b5d0eb9092152326a32c536a7438c09e23
Vojtěch Bakaj <bakajvo1@fit.cvut.cz>
2018-12-17 22:28:53 +0100

    SetUp Basic project

---
+++ Engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Engine appliaction
+
+#include "Engine\Application.h"
+
+// ------------Entry Point------------
+#include "Engine\EntryPoint.h"
+// -----------------------------------
